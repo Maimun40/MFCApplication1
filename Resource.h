8a59c8e712addb799e9cfb3d5c6cd02a840473d0
Maimun40 <steftanev@gmail.com>
2020-02-22 11:27:08 +0200

    Fixed #1

--- Resource.h
+++ Resource.h
@@ -7,14 +7,18 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_MFCAPPLICATION1_DIALOG      102
 #define IDR_MAINFRAME                   128
+#define IDC_LIST1                       1000
+#define IDC_TREE1                       1001
+#define IDC_SYSLINK1                    1002
+#define IDC_MFCMENUBUTTON1              1003
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
